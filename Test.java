e625c128d56414eff4539688575137211df9bfea
Craze_Biao <1538783305@qq.com>
2024-04-22 11:01:51 +0800

    Test_0001

---
+++ Test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Test {
+    public staic void main(String[] args){
+        Scanner input = new Scanner(System.in)
+        System.out.println("hello world！");
+        String test = input,nextLine();
+        System.out.println(test);
+    }
+}
